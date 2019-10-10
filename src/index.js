66f86cb66e3f78d4ae128cb4ef15974e24c9ba9a
kpwwalsh <wnuru@hotmail.com>
2019-10-10 12:48:26 -0500

    added port command to console.log

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const server = http.createServer(app);
 server.listen(process.env.PORT || 3000);
 
 server.on("listening", () => {
-  console.log("server is listening for requests on port 3000");
+  console.log('server is listening for requests on port ${process.env.PORT || 3000}');
 });
