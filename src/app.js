cf5628bb953b8a8887e16646fc1ccc4be5f1a246
kpwwalsh <wnuru@hotmail.com>
2019-10-15 11:04:27 -0500

    sign up function and tests not working

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 const logger = require('morgan');
-
-
 const routeConfig = require("./config/route-config.js");
 const appConfig = require('./config/main-config.js');
 
-appConfig.init();
+appConfig.init(app, express);
 routeConfig.init(app);
 app.use(logger('dev'));
 
---
+++ src/routes/validation.js
@@ -0,0 +1,28 @@
+const User = require('../db/models').User;
+
+module.exports = {
+    validateUsers(req, res, next) {
+  
+  //#1
+      if(req.method === "POST") {
+  
+  //#2
+        req.checkBody("username", "must be at least 6 characters in length").isLength({min: 2});
+        req.checkBody("email", "must be valid").isEmail();
+        req.checkBody("password", "must be 7 characters in length").isLength({min:7});
+        req.checkBody("passwordConfirmation", "must match password provided").matches(req.body.password());
+      }
+  
+  //#3
+      const errors = req.validationErrors();
+  
+      if (errors) {
+  
+  //#4
+        req.flash("error", errors);
+        return res.redirect(req.headers.referer)
+      } else {
+        return next();
+      }
+    }
+  }
