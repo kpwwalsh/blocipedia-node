94930aaf6a6f7c07b65693482e7cec8c3e48a7d2
kpwwalsh <wnuru@hotmail.com>
2019-10-18 17:01:57 -0500

    queries.users.js back

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,7 +25,7 @@ module.exports = {
               req.flash('notice', "You've successfully signed up!");
               emailConfirmation.sendEmail(newUser.email);
               res.redirect('/');
-            });
+            })();
       }
     });
   },
