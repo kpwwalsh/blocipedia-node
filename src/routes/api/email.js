30a7887d02184f720482777a7e3ac4a69f5c3687
kpwwalsh <wnuru@hotmail.com>
2019-10-22 10:33:17 -0500

    validation error prompt unchanged

--- src/routes/api/email.js
+++ src/routes/api/email.js
@@ -6,14 +6,13 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey('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');
 
 module.exports={ 
-    sendEmail(newUserMail){ 
+    sendEmail(newUserEmail){ 
       const msg = {
 
-    to: 'test@example.com',
-    from: 'test@example.com',
-     subject: 'Sending with Twilio SendGrid is Fun',
-     text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    to: 'newUserEmail',
+    from: 'test@no-reply.com',
+     subject: 'Blocipedia account',
+    html: '<p This email confirms successful Blocipedia sign-up/p>',
     };
     sgMail.send(msg);
     }
