7c008b3bc59dda11f5755e051597f2f252276e62
kpwwalsh <wnuru@hotmail.com>
2019-10-28 10:01:30 -0500

    initial push wiki crud

---
+++ spec/integration/wikis_spec.js
@@ -0,0 +1,158 @@
+// #1
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/wikis/";
+const Wiki= require('../../src/db/models').Wiki;
+const sequelize = require("../../src/db/models/index").sequelize;
+const User = require("../../src/db/models").User;
+
+
+describe("routes : wikis", () => {
+  beforeEach((done) => {
+    this.user;
+    this.wiki;
+    sequelize.sync({force: true}).then((res) => {
+      User.create({
+        username: 'joeblow',
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe",
+        userId: this.user.id   
+      })
+      .then((user) => {
+        this.user = user;  // store user
+
+        Wiki.create({
+            title: "Indochinese tiger",
+            body: "Only a few of them left",
+            userId: this.user.id   
+        })
+        .then((wiki) => {
+          this.wiki = wiki;                 // store topic
+            done();
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+    });
+
+    
+      describe("GET /wikis", () => {
+       
+        it("should return a status code 200 and all wikis", (done) => {
+          request.get(base, (err, res, body) => {
+            expect(err).toBeNull();
+            expect(body).toContain("Wikis");
+            expect(body).toContain("Indochinese tiger");
+            done();
+          });
+        });
+      });
+
+      describe("GET /wikis/new", () => {
+       
+        it("should render a new wiki form", (done) => {
+          request.get(`${base}new`, (err, res, body) => {
+            expect(err).toBeNull();
+            expect(body).toContain("New Wiki");
+            done();
+          });
+        });
+      });
+    
+      describe('POST /wikis/create', () => {
+      
+        it('should create a new wiki and redirect', done => {
+          const options = {
+            url: '${base}create',
+            form: {
+              title:"Indochinese tiger",
+              body:"Only a few of them left",
+              userId: this.user.id
+            }
+          };
+          request.post(options, (err, res, body) => {
+            Wiki.findOne({ where: { title: 'Indochinese tiger' } })
+              .then(wiki => {
+                expect(wiki.title).toBe("Indochinese tiger");
+                expect(wiki.body).toBe("Only a few of them left");
+                done();
+              })
+              .catch(err => {
+                console.log(err);
+                done();
+              });
+          });
+        });
+    });
+     
+        describe('GET /wikis/:id', () => {
+          it('should render a view with the selected wiki', done => {
+           request.get(`${base}${this.wiki.id}`, (err, res, body) => {
+            expect(err).toBeNull();
+            expect(body).toContain('Indochinese tiger');
+            done();
+          });
+        });
+      });
+
+      describe("POST /wikis/:id/destroy", () => {
+        it("should delete the wiki with the associated ID", (done) => {
+          Wiki.findAll()
+          .then((wikis) => {
+            const wikiCountBeforeDelete = wikis.length;
+            expect(wikiCountBeforeDelete).toBe(1);
+            request.post(
+             `${base}${this.wiki.id}/destroy`,
+              (err, res, body) => {
+              Wiki.findAll()
+              .then((wikis) => {
+                expect(err).toBeNull();
+                expect(wikis.length).toBe(wikiCountBeforeDelete - 1);
+                done();
+              }) 
+            });
+          })
+        }); 
+      });
+  describe("GET /wikis/:id/edit", () => {
+    it("should render a view with an edit wiki form", (done) => {
+      request.get(`${base}${this.wiki.id}/edit`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Edit Wiki");
+        expect(body).toContain("Indochinese tiger");
+        done();
+      });
+    });
+  });
+
+  describe("POST /wikis/:id/update", () => {
+    it("should update the wiki with the given values", (done) => {
+      request.post({
+        url: `${base}${this.wiki.id}/update`,
+        form: {
+          title: "Indochinese tiger",
+          body: "Only a few of them left",
+          userId: this.user.id
+        }
+      }, (err, res, body) => {
+        expect(err).toBeNull();
+        Wiki.findOne({
+          where: {id:1}
+        })
+        .then((wiki) => {
+          expect(wiki.title).toBe("Indochinese tiger");
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+    });
+  });
+});
+   
+    
---
+++ spec/unit/wiki_spec.js
@@ -0,0 +1,121 @@
+const sequelize = require("../../src/db/models/index").sequelize;
+const User = require('../../src/db/models').User;
+const Wiki=require('../../src/db/models').Wiki;
+
+
+describe("Wiki", () => {
+  beforeEach((done) => {
+    this.user;
+    this.wiki;
+    sequelize.sync({force: true}).then((res) => {
+      User.create({
+        username: 'joeblow',
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe",
+        userId:this.user.id
+      })
+      .then((user) => {
+        this.user = user;
+
+        Wiki.create({
+            title: "Indochinese tiger",
+            body: "Only a few of them left",
+            userId: this.user.id
+          })
+          .then((wiki) => {
+            this.wiki= wiki;
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+    });
+  });
+
+// #4: We start a test suite for the `create` action
+  describe("#create()", () => {
+
+    it("should create a wiki object with title and a body", (done) => {
+      Wiki.create({                // create a comment
+        title: "Indochinese tiger",
+        body: "Only a few of them left",
+        userId: this.user.id
+      })
+      .then((wiki) => {            // confirm it was created with the values passed
+        expect(wiki.title).toBe("Indochinese tiger");
+        expect(wiki.body).toBe("Only a few of them left");
+        done();
+
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+
+
+// #5: We test that comments with invalid attributes are not created
+    it("should not create a wiki with missing title or description", (done) => {
+      Wiki.create({
+        title: "Do Androids Dream of Electric Sheep?"
+      })
+      .then((wiki) => {
+
+        // the code in this block will not be evaluated since the validation error
+        // will skip it. Instead, we'll catch the error in the catch block below
+        // and set the expectations there
+
+        done();
+
+      })
+      .catch((err) => {
+        expect(err.message).toContain("Wiki.body cannot be null");
+        done();
+
+      })
+    });
+
+  });
+
+// #6: We test the `setUser` method which assigns a User object to the comment it was called on
+  describe("#setUser()", () => {
+    it("should associate a wiki and a user together", (done) => {
+      User.create({               // create an unassociated user
+        username: 'zippydave',
+        email: "bob@example.com",
+        password: "1234567",
+        passwordConfirmation: '1234567'
+      })
+      .then((newUser) => {        // pass the user down
+        expect(this.wiki.userId).toBe(this.user.id); // confirm the comment belongs to another user
+        this.wiki.setUser(newUser)                   // then reassign it
+        .then((wiki) => {
+          expect(wiki.userId).toBe(newUser.id);      // confirm the values persisted
+          done();
+
+        });
+      })
+    });
+
+  });
+
+// #7: We test the `getUser` method which should return the User associated with the comment called on
+  describe("#getUser()", () => {
+    it("should return the associated user", (done) => {
+      this.wiki.getUser()
+      .then((associatedUser) => {
+        expect(associatedUser.email).toBe("starman@tesla.com");
+        done();
+      });
+
+    });
+
+  });
+
